3864428a583a172181418f0d36911eed039e2fb1
cutelixin <792075865@qq.com>
2023-12-19 22:15:49 +0800

    提交webAPI代码和练习

---
+++ 03_JavaScript/02_webAPI/04-webAPIs/04-code/综合作业素材/data/data.js
@@ -0,0 +1,42 @@
+let data = [
+    {
+        src: './综合作业素材/images/course01.png',
+        title: 'Think PHP 5.0 ����ϵͳʵս��Ŀ����',
+        num: 1125
+    },
+    {
+        src: './综合作业素材/images/course02.png',
+        title: 'Android ���綯̬ͼƬ����ʵս',
+        num: 357
+    },
+    {
+        src: './综合作业素材/images/course03.png',
+        title: 'Angular2 ��ǰ���̳�ʵս��Ŀ����',
+        num: 22250
+    },
+    {
+        src: './综合作业素材/images/course04.png',
+        title: 'Android APP ʵս��Ŀ����',
+        num: 389
+    },
+    {
+        src: './综合作业素材/images/course05.png',
+        title: 'UGUI Դ����ȷ�������',
+        num: 124
+    },
+    {
+        src: './综合作业素材/images/course06.png',
+        title: 'Kami2��ҳ�����л�Ч��ʵս����',
+        num: 432
+    },
+    {
+        src: './综合作业素材/images/course07.png',
+        title: 'UNITY �����ŵ���ͨʵս����',
+        num: 888
+    },
+    {
+        src: './综合作业素材/images/course08.png',
+        title: 'Cocos ���ѧϰ�㲻�������ʵս',
+        num: 590
+    },
+]
